58f024a7d124e29b3b97f04d27327f58bdcac83d
verdantpath <m_dittmann@yahoo.com>
2018-02-17 21:25:35 -0800

    update functions.php

--- functions.php
+++ functions.php
@@ -24,6 +24,17 @@ function theme_js() {
 }
 add_action( 'wp_enqueue_scripts', 'theme_js' );
 
+// add_filter( 'show_admin_bar', '__return_false' );
 
+add_theme_support( 'menus' );
+
+function register_theme_menus() {
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' )
+    )
+  );
+}
+add_action( 'init', 'register_theme_menus' );
 
 ?>
