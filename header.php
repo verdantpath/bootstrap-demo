f6c65f3e44f9ff50de193eefcdb64e3e24d90a6c
verdantpath <m_dittmann@yahoo.com>
2018-02-21 20:47:32 -0800

    update header.php

--- header.php
+++ header.php
@@ -4,9 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../../../favicon.ico">
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico">
 
     <title>Jumbotron Template for Bootstrap</title>
 
