cf9671fcee1fbe44e7b6ab7aafcf3f46e8426671
verdantpath <m_dittmann@yahoo.com>
2018-02-25 20:05:06 -0800

    update functions.php

--- functions.php
+++ functions.php
@@ -37,4 +37,20 @@ function register_theme_menus() {
 }
 add_action( 'init', 'register_theme_menus' );
 
+function create_widget($name, $id, $description) {
+  register_sidebar(array(
+    'name' => __( $name ),
+    'id' => $id,
+    'description' => __( $description ),
+    'before_widget' => 'div class="widget">',
+    'after_widget' => '</div>',
+    'before_title' => '<h3>',
+    'after_title' => '</h3>'
+  ));
+}
+
+create_widget( 'Front Page Left', 'front-left', 'Displays on the left of the Home page' );
+create_widget( 'Front Page Left', 'front-left', 'Displays on the left of the Home page' );
+create_widget( 'Front Page Left', 'front-left', 'Displays on the left of the Home page' );
+
 ?>
