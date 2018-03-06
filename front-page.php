8cff230e29d24ba753a2c73dee3dfa99894d2a8d
verdantpath <m_dittmann@yahoo.com>
2018-03-05 20:38:41 -0800

    update front-page.php

--- front-page.php
+++ front-page.php
@@ -22,14 +22,10 @@
             <?php if( dynamic_sidebar( 'front-left' ) ); ?>
           </div>
           <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            <?php if( dynamic_sidebar( 'front-center' ) ); ?>
           </div>
           <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            <?php if( dynamic_sidebar( 'front-right' ) ); ?>
           </div>
         </div>
 
