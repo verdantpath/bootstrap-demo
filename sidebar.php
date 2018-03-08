bdba78542a88505796af63788a7de1b3348825a2
verdantpath <m_dittmann@yahoo.com>
2018-03-07 22:32:15 -0800

    add sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,4 @@
-<div class="col-md-3">
-  <h2>Sidebar</h2>
-  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-  <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+<div class="col-md-3 sidebar">
+  <h2>Sidebar Setup</h2>
+  <p>Please add widgets to the sidebar to have them dispaly here. </p>
 </div>
