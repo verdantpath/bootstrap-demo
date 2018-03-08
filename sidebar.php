2c68475c1b7f625efa5511112d848e1803f92ed4
verdantpath <m_dittmann@yahoo.com>
2018-03-07 22:55:41 -0800

    update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,9 @@
 <div class="col-md-3 sidebar">
+
+  <?php if ( ! dynamic_sidebar( 'page' ) ) : ?>
+
   <h2>Sidebar Setup</h2>
   <p>Please add widgets to the sidebar to have them dispaly here. </p>
+
+  <?php endif; ?>
 </div>
