f4ce822be9d2d2f8c5723711ff6765455884eed7
verdantpath <m_dittmann@yahoo.com>
2018-03-07 21:54:26 -0800

    add sidebar.php, update page.php

--- page.php
+++ page.php
@@ -25,11 +25,7 @@
 
           </div>
 
-          <div class="col-md-3">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-          </div>
+          <?php get_sidebar(); ?>
 
         </div>
 
