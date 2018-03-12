6e932827974a5d5c3e22ce3245c01b3c0201cb4e
verdantpath <m_dittmann@yahoo.com>
2018-03-11 21:32:35 -0700

    update home.php

--- home.php
+++ home.php
@@ -5,6 +5,10 @@
 
           <div class="col-md-9">
 
+            <div class="page-header">
+              <h1><?php wp_title(''); ?></h1>
+            </div>
+
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
               <div class="page-header">
