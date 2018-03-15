4dad92c19ab07b8be11da6918cff9fdac01a605a
verdantpath <m_dittmann@yahoo.com>
2018-03-14 22:08:05 -0700

    update single.php

--- single.php
+++ single.php
@@ -5,21 +5,13 @@
 
           <div class="col-md-9">
 
-            <div class="page-header">
-              <h1><?php wp_title(''); ?></h1>
-            </div>
-
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-              <article class="post">
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <p><em>by <?php the_author(); ?> on <?php echo the_time('l, F jS, Y'); ?> in <?php the_category( ', ' ); ?> |  <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a></em></p>
+              <div class="page-header">
+                <h1><?php the_title(); ?></h1>
+              </div>
 
-                <?php the_excerpt(); ?>
-
-                <hr>
-
-              </article>
+              <?php the_content(); ?>
 
             <?php endwhile; else : ?>
 
@@ -33,7 +25,7 @@
 
           </div>
 
-          <?php get_sidebar( 'blog' ); ?>
+          <?php get_sidebar(); ?>
 
         </div>
 
