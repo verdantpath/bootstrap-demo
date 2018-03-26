d668dc733a6eaf176baf1ef545951b6def85a9f8
verdantpath <m_dittmann@yahoo.com>
2018-03-25 21:25:07 -0700

    add page-portfolio.php

---
+++ page-portfolio.php
@@ -0,0 +1,58 @@
+<?php
+  /*
+  Template Name: Portfolio Grid
+
+  */
+?>
+
+<?php get_header(); ?>
+
+      <div class="container">
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+              <div class="page-header">
+                <h1><?php the_title(); ?></h1>
+              </div>
+
+              <?php the_content(); ?>
+
+            <?php endwhile; else : ?>
+
+              <div class="page-header">
+                <h1>Oh no!</h1>
+              </div>
+
+              <p>No content is appearing for this page!</p>
+
+            <?php endif; ?>
+
+          </div>
+
+        </div>
+
+        <div class="row">
+
+          <?php
+            $args = array(
+              'post_type' => 'portfolio'
+            );
+            $the_query = new WP_Query( $args );
+          ?>
+
+          <?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post() ?>
+
+          <div class="col-sm-3 portfolio-piece">
+
+            <h3><?php the_title(); ?></h3>
+
+          </div>
+
+          <?php endwhile; endif; ?>
+
+        </div>
+
+<?php get_footer(); ?>
