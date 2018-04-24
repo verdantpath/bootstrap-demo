fa042057ce842bbf3eba73453cc509d850777a94
verdantpath <m_dittmann@yahoo.com>
2018-04-23 22:46:32 -0700

    update single-portfolio.php

--- single-portfolio.php
+++ single-portfolio.php
@@ -20,7 +20,7 @@
         <div class="row">
 
 
-
+            <!-- the wordpress loop -->
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
               <div class="col-sm-8 portfolio-image">
