1279949844f579846cde75b787e38e8d69ae7b79
verdantpath <m_dittmann@yahoo.com>
2018-05-01 21:57:04 -0700

    update index.php

--- index.php
+++ index.php
@@ -1,34 +1,32 @@
 <?php get_header(); ?>
 
-    <main role="main">
-
-      <!-- Main jumbotron for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <div class="container">
-          <h1 class="display-3">Hello, world!</h1>
-          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
-        </div>
-      </div>
-
       <div class="container">
-        <!-- Example row of columns -->
         <div class="row">
-          <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+
+          <div class="col-md-9">
+
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+              <div class="page-header">
+                <h1><?php the_title(); ?></h1>
+              </div>
+
+              <?php the_content(); ?>
+
+            <?php endwhile; else : ?>
+
+              <div class="page-header">
+                <h1>Oh no!</h1>
+              </div>
+
+              <p>No content is appearing for this page!</p>
+
+            <?php endif; ?>
+
           </div>
-          <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div>
-          <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div>
+
+          <?php get_sidebar(); ?>
+
         </div>
 
 <?php get_footer(); ?>
