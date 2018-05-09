cc6e22736850b6b291d877f81fac805cef07e98f
verdantpath <m_dittmann@yahoo.com>
2018-05-08 21:03:08 -0700

    update home.php

--- home.php
+++ home.php
@@ -13,8 +13,6 @@
               <!-- Indicators -->
               <ol class="carousel-indicators">
                 <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
               </ol>
 
               <!-- Wrapper for slides -->
